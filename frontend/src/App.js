4fa5a2bba576a4c26e7d035918b7aa70b0838040
Erika Torio <2160120@slu.edu.ph>
2021-01-14 11:32:16 +0800

    minor fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,10 +15,9 @@ function App() {
     <Router>
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="https://google.com" target="_blank">
+          <a class="navbar-brand" href="/">
             <img src={ logo } width="30" height="30" alt=""/>
           </a>
-          <Link to="/" className="navbar-brand">To-Do App</Link>
           <div className="collapse navbar-collapse">
           <ul className="navbar-nav mr-auto">
                 <li className="navbar-item">
